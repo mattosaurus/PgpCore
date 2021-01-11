36c48343467dbe43056419a2be7c504458f8e95e
mattosaurus <bitscry@outlook.com>
2021-01-11 12:07:37 +0000

    Add async stream methods

--- PgpCore/StreamHelper.cs
+++ PgpCore/StreamHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PgpCore
 {
@@ -15,10 +16,27 @@ namespace PgpCore
 			return stream;
 		}
 
+		public static async Task<Stream> GetStreamAsync(this string s, Encoding encoding = null)
+		{
+			var stream = new MemoryStream();
+			var writer = encoding != null ? new StreamWriter(stream, encoding) : new StreamWriter(stream);
+			await writer.WriteAsync(s);
+			await writer.FlushAsync();
+			stream.Position = 0;
+			return stream;
+		}
+
 		public static string GetString(this Stream inputStream)
 		{
 			var reader = new StreamReader(inputStream);
 			var       output = reader.ReadToEnd();
+			return output;
+		}
+
+		public static async Task<string> GetStringAsync(this Stream inputStream)
+		{
+			var reader = new StreamReader(inputStream);
+			var output = await reader.ReadToEndAsync();
 			return output;
 		}
 	}
