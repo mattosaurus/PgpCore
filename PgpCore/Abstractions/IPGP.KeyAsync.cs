781c55f2207699d2d0721921f9ee850432826cc8
mattosaurus <mattosaurus@outlook.com>
2023-12-05 17:38:58 +0000

    Restore key async methods

---
+++ PgpCore/Abstractions/IPGP.KeyAsync.cs
@@ -0,0 +1,39 @@
+﻿using Org.BouncyCastle.Bcpg;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PgpCore.Abstractions
+{
+    public interface IKeyAsync
+    {
+        Task GenerateKeyAsync(
+            FileInfo publicKeyFileInfo,
+            FileInfo privateKeyFileInfo,
+            string username = null,
+            string password = null,
+            int strength = 1024,
+            int certainty = 8,
+            bool armor = true,
+            bool emitVersion = true,
+            long keyExpirationInSeconds = 0,
+            long signatureExpirationInSeconds = 0,
+            CompressionAlgorithmTag[] preferredCompressionAlgorithms = null,
+            HashAlgorithmTag[] preferredHashAlgorithmTags = null,
+            SymmetricKeyAlgorithmTag[] preferredSymetricKeyAlgorithms = null);
+
+        Task GenerateKeyAsync(
+            Stream publicKeyStream,
+            Stream privateKeyStream,
+            string username = null,
+            string password = null,
+            int strength = 1024,
+            int certainty = 8,
+            bool armor = true,
+            bool emitVersion = true,
+            long keyExpirationInSeconds = 0,
+            long signatureExpirationInSeconds = 0,
+            CompressionAlgorithmTag[] preferredCompressionAlgorithms = null,
+            HashAlgorithmTag[] preferredHashAlgorithmTags = null,
+            SymmetricKeyAlgorithmTag[] preferredSymetricKeyAlgorithms = null);
+    }
+}
