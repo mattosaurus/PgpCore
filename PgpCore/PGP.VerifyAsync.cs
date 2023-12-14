2b27119b1dd27c0216f03ad9131abaa63cbfef84
mattosaurus <mattosaurus@outlook.com>
2023-12-14 16:36:55 +0000

    Write directly to stream

--- PgpCore/PGP.VerifyAsync.cs
+++ PgpCore/PGP.VerifyAsync.cs
@@ -63,106 +63,103 @@ namespace PgpCore
             if (outputStream == null)
                 outputStream = new MemoryStream();
 
-            using (StreamWriter contentStreamWriter = new StreamWriter(outputStream, inputStream.GetEncoding(), 1024, true))
+            inputStream.Seek(0, SeekOrigin.Begin);
+            Stream encodedFile = PgpUtilities.GetDecoderStream(inputStream);
+            PgpObjectFactory factory = new PgpObjectFactory(encodedFile);
+            PgpObject pgpObject = factory.NextPgpObject();
+
+            if (pgpObject is PgpCompressedData)
             {
-                inputStream.Seek(0, SeekOrigin.Begin);
-                Stream encodedFile = PgpUtilities.GetDecoderStream(inputStream);
-                PgpObjectFactory factory = new PgpObjectFactory(encodedFile);
-                PgpObject pgpObject = factory.NextPgpObject();
+                PgpPublicKeyEncryptedData publicKeyEncryptedData = Utilities.ExtractPublicKeyEncryptedData(encodedFile);
 
-                if (pgpObject is PgpCompressedData)
-                {
-                    PgpPublicKeyEncryptedData publicKeyEncryptedData = Utilities.ExtractPublicKeyEncryptedData(encodedFile);
-
-                    // Verify against public key ID and that of any sub keys
-                    var keyIdToVerify = publicKeyEncryptedData.KeyId;
-                    verified = Utilities.FindPublicKey(keyIdToVerify, EncryptionKeys.VerificationKeys,
-                        out PgpPublicKey _);
-                }
-                else if (pgpObject is PgpEncryptedDataList dataList)
-                {
-                    if (throwIfEncrypted)
-                    {
-                        throw new ArgumentException("Input is encrypted. Decrypt the input first.");
-                    }
-                    PgpPublicKeyEncryptedData publicKeyEncryptedData = Utilities.ExtractPublicKey(dataList);
-                    var keyIdToVerify = publicKeyEncryptedData.KeyId;
-                    // If we encounter an encrypted packet, verify with the encryption keys used instead
-                    // TODO does this even make sense? maybe throw exception instead, or try to decrypt first
-                    verified = Utilities.FindPublicKeyInKeyRings(keyIdToVerify, EncryptionKeys.PublicKeyRings.Select(keyRing => keyRing.PgpPublicKeyRing), out PgpPublicKey _);
-                }
-                else if (pgpObject is PgpOnePassSignatureList onePassSignatureList)
-                {
-                    PgpOnePassSignature pgpOnePassSignature = onePassSignatureList[0];
-                    PgpLiteralData pgpLiteralData = (PgpLiteralData)factory.NextPgpObject();
-                    Stream pgpLiteralStream = pgpLiteralData.GetInputStream();
-
-                    // Verify against public key ID and that of any sub keys
-                    var keyIdToVerify = pgpOnePassSignature.KeyId;
-                    if (Utilities.FindPublicKey(keyIdToVerify, EncryptionKeys.VerificationKeys,
-                            out PgpPublicKey validationKey))
-                    {
-                        pgpOnePassSignature.InitVerify(validationKey);
-
-                        int ch;
-                        while ((ch = pgpLiteralStream.ReadByte()) >= 0)
-                        {
-                            pgpOnePassSignature.Update((byte)ch);
-                            await contentStreamWriter.WriteAsync((char)ch);
-                        }
-
-                        PgpSignatureList pgpSignatureList = (PgpSignatureList)factory.NextPgpObject();
-
-                        for (int i = 0; i < pgpSignatureList.Count; i++)
-                        {
-                            PgpSignature pgpSignature = pgpSignatureList[i];
-
-                            if (pgpOnePassSignature.Verify(pgpSignature))
-                            {
-                                verified = true;
-                                break;
-                            }
-                        }
-                    }
-                }
-                else if (pgpObject is PgpSignatureList signatureList)
-                {
-                    PgpSignature pgpSignature = signatureList[0];
-                    PgpLiteralData pgpLiteralData = (PgpLiteralData)factory.NextPgpObject();
-                    Stream pgpLiteralStream = pgpLiteralData.GetInputStream();
-
-                    // Verify against public key ID and that of any sub keys
-                    if (Utilities.FindPublicKey(pgpSignature.KeyId, EncryptionKeys.VerificationKeys,
-                            out PgpPublicKey publicKey))
-                    {
-                        foreach (PgpSignature _ in publicKey.GetSignatures())
-                        {
-                            if (!verified)
-                            {
-                                pgpSignature.InitVerify(publicKey);
-
-                                int ch;
-                                while ((ch = pgpLiteralStream.ReadByte()) >= 0)
-                                {
-                                    pgpSignature.Update((byte)ch);
-                                    await contentStreamWriter.WriteAsync((char)ch);
-                                }
-
-                                verified = pgpSignature.Verify();
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-                    }
-                }
-                else
-                    throw new PgpException("Message is not a encrypted and signed file or simple signed file.");
-
-                await contentStreamWriter.FlushAsync();
-                outputStream.Seek(0, SeekOrigin.Begin);
+                // Verify against public key ID and that of any sub keys
+                var keyIdToVerify = publicKeyEncryptedData.KeyId;
+                verified = Utilities.FindPublicKey(keyIdToVerify, EncryptionKeys.VerificationKeys,
+                    out PgpPublicKey _);
             }
+            else if (pgpObject is PgpEncryptedDataList dataList)
+            {
+                if (throwIfEncrypted)
+                {
+                    throw new ArgumentException("Input is encrypted. Decrypt the input first.");
+                }
+                PgpPublicKeyEncryptedData publicKeyEncryptedData = Utilities.ExtractPublicKey(dataList);
+                var keyIdToVerify = publicKeyEncryptedData.KeyId;
+                // If we encounter an encrypted packet, verify with the encryption keys used instead
+                // TODO does this even make sense? maybe throw exception instead, or try to decrypt first
+                verified = Utilities.FindPublicKeyInKeyRings(keyIdToVerify, EncryptionKeys.PublicKeyRings.Select(keyRing => keyRing.PgpPublicKeyRing), out PgpPublicKey _);
+            }
+            else if (pgpObject is PgpOnePassSignatureList onePassSignatureList)
+            {
+                PgpOnePassSignature pgpOnePassSignature = onePassSignatureList[0];
+                PgpLiteralData pgpLiteralData = (PgpLiteralData)factory.NextPgpObject();
+                Stream pgpLiteralStream = pgpLiteralData.GetInputStream();
+
+                // Verify against public key ID and that of any sub keys
+                var keyIdToVerify = pgpOnePassSignature.KeyId;
+                if (Utilities.FindPublicKey(keyIdToVerify, EncryptionKeys.VerificationKeys,
+                        out PgpPublicKey validationKey))
+                {
+                    pgpOnePassSignature.InitVerify(validationKey);
+
+                    int ch;
+                    while ((ch = pgpLiteralStream.ReadByte()) >= 0)
+                    {
+                        pgpOnePassSignature.Update((byte)ch);
+                        outputStream.WriteByte((byte)ch);
+                    }
+
+                    PgpSignatureList pgpSignatureList = (PgpSignatureList)factory.NextPgpObject();
+
+                    for (int i = 0; i < pgpSignatureList.Count; i++)
+                    {
+                        PgpSignature pgpSignature = pgpSignatureList[i];
+
+                        if (pgpOnePassSignature.Verify(pgpSignature))
+                        {
+                            verified = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (pgpObject is PgpSignatureList signatureList)
+            {
+                PgpSignature pgpSignature = signatureList[0];
+                PgpLiteralData pgpLiteralData = (PgpLiteralData)factory.NextPgpObject();
+                Stream pgpLiteralStream = pgpLiteralData.GetInputStream();
+
+                // Verify against public key ID and that of any sub keys
+                if (Utilities.FindPublicKey(pgpSignature.KeyId, EncryptionKeys.VerificationKeys,
+                        out PgpPublicKey publicKey))
+                {
+                    foreach (PgpSignature _ in publicKey.GetSignatures())
+                    {
+                        if (!verified)
+                        {
+                            pgpSignature.InitVerify(publicKey);
+
+                            int ch;
+                            while ((ch = pgpLiteralStream.ReadByte()) >= 0)
+                            {
+                                pgpSignature.Update((byte)ch);
+                                outputStream.WriteByte((byte)ch);
+                            }
+
+                            verified = pgpSignature.Verify();
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+                throw new PgpException("Message is not a encrypted and signed file or simple signed file.");
+
+            outputStream.Flush();
+            outputStream.Seek(0, SeekOrigin.Begin);
 
             return (verified);
         }
