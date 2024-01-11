8abc310764fce4ccbc2749c96cb7843236a963d7
mattosaurus <mattosaurus@outlook.com>
2024-01-11 15:55:10 +0000

    Add test helpers

---
+++ PgpCore.Tests/TestHelper.cs
@@ -0,0 +1,46 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+using Org.BouncyCastle.Bcpg;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PgpCore.Tests
+{
+    internal static class TestHelper
+    {
+        internal static PgpPublicKey ReadPublicKey(Stream inputStream)
+        {
+            PgpPublicKeyRingBundle pgpPub = new PgpPublicKeyRingBundle(PgpUtilities.GetDecoderStream(inputStream));
+            foreach (PgpPublicKeyRing kRing in pgpPub.GetKeyRings())
+            {
+                foreach (PgpPublicKey k in kRing.GetPublicKeys())
+                {
+                    if (k.IsEncryptionKey)
+                        return k;
+                }
+            }
+            throw new ArgumentException("No encryption key found in public key ring.");
+        }
+
+        internal static IEnumerable<T> GetEnumValues<T>() where T : struct, IConvertible
+        {
+            foreach (T enumValue in Enum.GetValues(typeof(T)))
+            {
+                yield return enumValue;
+            }
+        }
+
+        internal static IEnumerable<object[]> GetAllCombinations()
+        {
+            foreach (CompressionAlgorithmTag compressionAlgorithmTag in GetEnumValues<CompressionAlgorithmTag>())
+                foreach (HashAlgorithmTag hashAlgorithmTag in GetEnumValues<HashAlgorithmTag>())
+                    foreach (SymmetricKeyAlgorithmTag symmetricKeyAlgorithmTag in GetEnumValues<SymmetricKeyAlgorithmTag>())
+                    {
+                        yield return new object[] { compressionAlgorithmTag, hashAlgorithmTag, symmetricKeyAlgorithmTag };
+                    }
+        }
+    }
+}
---
+++ PgpCore.Tests/UnitTests/TestBase.cs
@@ -0,0 +1,40 @@
+﻿using Org.BouncyCastle.Bcpg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PgpCore.Tests.UnitTests
+{
+    public class TestBase
+    {
+        public static IEnumerable<object[]> GetCompressionAlgorithimTags()
+        {
+            foreach (CompressionAlgorithmTag compressionAlgorithmTag in TestHelper.GetEnumValues<CompressionAlgorithmTag>())
+            {
+                yield return new object[] { compressionAlgorithmTag };
+            }
+        }
+
+        public static IEnumerable<object[]> GetHashAlgorithimTags()
+        {
+            foreach (HashAlgorithmTag hashAlgorithmTag in TestHelper.GetEnumValues<HashAlgorithmTag>())
+            {
+                yield return new object[] { hashAlgorithmTag };
+            }
+        }
+
+        public static IEnumerable<object[]> GetSymmetricAlgorithimTags()
+        {
+            foreach (SymmetricKeyAlgorithmTag symmetricKeyAlgorithmTag in TestHelper.GetEnumValues<SymmetricKeyAlgorithmTag>())
+            {
+                // Exclude as null is not for encryption and safer is not supported.
+                if (symmetricKeyAlgorithmTag == SymmetricKeyAlgorithmTag.Null || symmetricKeyAlgorithmTag == SymmetricKeyAlgorithmTag.Safer)
+                    continue;
+
+                yield return new object[] { symmetricKeyAlgorithmTag };
+            }
+        }
+    }
+}
