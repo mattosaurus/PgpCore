71c90ac10c907523ebc12dc3101f008afc3bb20f
mattosaurus <mattosaurus@outlook.com>
2024-06-10 13:21:05 +0100

    Add key tests

--- PgpCore.Tests/UnitTests/GenerateKey/KeySync.cs
+++ PgpCore.Tests/UnitTests/GenerateKey/KeySync.cs
@@ -289,5 +289,77 @@ namespace PgpCore.Tests.UnitTests.GenerateKey
                 }
             }
         }
+
+        [Theory]
+        [InlineData(PublicKeyAlgorithmTag.RsaGeneral)]
+        [InlineData(PublicKeyAlgorithmTag.ElGamalGeneral)]
+        public void GenerateKey_CreatePublicAndPrivateKeysWithDifferentPublicKeyAlgorithms_ShouldCreateKeysWithSpecifiedAlgorithms(PublicKeyAlgorithmTag publicKeyAlgorithm)
+        {
+            // Arrange
+            TestFactory testFactory = new TestFactory();
+            testFactory.Arrange();
+            PGP pgp = new PGP();
+            pgp.PublicKeyAlgorithm = publicKeyAlgorithm;
+
+            // Act
+            pgp.GenerateKey(
+                testFactory.PublicKeyFileInfo,
+                testFactory.PrivateKeyFileInfo,
+                testFactory.UserName,
+                testFactory.Password
+                );
+
+            // Assert
+            // Assert that the keys were created
+            using (new AssertionScope())
+            {
+                testFactory.PublicKeyFileInfo.Exists.Should().BeTrue();
+                testFactory.PrivateKeyFileInfo.Exists.Should().BeTrue();
+            }
+
+            // Assert public key properties
+            using (new AssertionScope())
+            {
+                File.ReadAllText(testFactory.PublicKeyFileInfo.FullName).Should().Contain(VERSION);
+
+                using (Stream publicKeyStream = testFactory.PublicKeyFileInfo.OpenRead())
+                {
+                    PgpPublicKey publicKey = publicKey = ReadPublicKey(publicKeyStream);
+                    // If we successfully read the public key without exceptions, it is considered valid
+                    publicKey.Should().NotBeNull();
+                    publicKey.Version.Should().Be(4);
+                    publicKey.CreationTime.Should().BeCloseTo(DateTime.UtcNow, new TimeSpan(0, 0, 10));
+                    publicKey.IsEncryptionKey.Should().BeTrue();
+                    publicKey.IsMasterKey.Should().BeTrue();
+                    publicKey.IsRevoked().Should().BeFalse();
+                    publicKey.BitStrength.Should().Be(1024);
+                }
+
+            }
+
+            // Assert private key properties
+            using (new AssertionScope())
+            {
+                File.ReadAllText(testFactory.PrivateKeyFileInfo.FullName).Should().Contain(VERSION);
+
+                using (Stream privateKeyStream = testFactory.PrivateKeyFileInfo.OpenRead())
+                {
+                    PgpSecretKeyRingBundle pgpSec = new PgpSecretKeyRingBundle(PgpUtilities.GetDecoderStream(privateKeyStream));
+                    foreach (PgpSecretKeyRing kRing in pgpSec.GetKeyRings())
+                    {
+                        foreach (PgpSecretKey k in kRing.GetSecretKeys())
+                        {
+                            if (k.IsSigningKey)
+                            {
+                                k.Should().NotBeNull();
+                                k.IsSigningKey.Should().BeTrue();
+                                k.IsMasterKey.Should().BeTrue();
+                                k.KeyEncryptionAlgorithm.Should().Be(SymmetricKeyAlgorithmTag.TripleDes);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
